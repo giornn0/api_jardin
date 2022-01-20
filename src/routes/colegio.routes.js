f8495f1a3fb14f506010f70c757ae263214918a0
gabikohen <gabokohen@gmail.com>
2022-01-20 15:45:55 -0300

    actulizando middlewares

--- src/routes/colegio.routes.js
+++ src/routes/colegio.routes.js
@@ -1,14 +1,14 @@
 import express from'express';
 import {getAllColegios,createColegio,editColegio,deleteColegio} from "../controllers/colegios.controller.js"
 import {upload} from "../middlewares/multer.js"
+import { contact } from '../middlewares/contact.js';
 const router = express.Router();
 
-router.get("/",(req,res,next)=>{
-  console.log(req)
-  next()
-}, getAllColegios)
+router.get("/", getAllColegios)
 router.post('/',upload.single('image'), createColegio);
 router.put('/:id',editColegio);
 router.delete('/:id', deleteColegio);
+
+router.post('/:id/contacto',contact)
 
 export default router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,8 @@
 import express from'express';
 import {editUser,deleteUser} from "../controllers/users.controller.js"
-import { contact } from '../middlewares/contact.js';
 const router = express.Router();
 
-<<<<<<< HEAD
-router.post('/:id/contacto',contact)
-router.put('/:id',editUser);
-=======
 router.put('/:id', editUser);
->>>>>>> a88bdad4694a9607cad4c46d81449a68ab34b819
 router.delete('/:id',deleteUser);
 
 export default router;
