3f2e08418c2624c0c455fbfde2c699951ecf9c6f
gabikohen <gabokohen@gmail.com>
2022-01-22 17:19:35 -0300

    middlewares

---
+++ src/middlewares/authColegio.js
@@ -0,0 +1,5 @@
+/*Buscar en la base si el usurio coincide com
+  con el que esta en session */
+  import { validationResult } from 'express-validator';
+
+
--- src/middlewares/colegio.js
+++ src/middlewares/colegio.js
@@ -1,1 +1,14 @@
-/*  hacer el */
+import {validate} from "./validateFn.js"
+import { body } from 'express-validator';
+
+export const colegio = validate([
+    body('nombre').notEmpty().isLength({min:4,max:30}),
+    body('estado').notEmpty().isBoolean(),
+    body('domicilio').notEmpty(),
+    body('email').notEmpty().withMessage('Escriba un email valido').bail(),
+    body('pass').notEmpty().isLength({ min: 6 ,max:30}).bail(),
+    body('telefono').isLength({ min: 6,max:12 }),
+    body('tipo').isLength({min:4,max:5}),
+  ])
+  
+  
